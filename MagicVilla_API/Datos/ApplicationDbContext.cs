9f053ebff3f671a64b54368bb7143e347c774bcb
Gerald Then <gthen@mdsoft.com.do>
2023-04-23 17:53:54 -0400

    Initial Crud and Create DataBase

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Villa>().HasIndex(x =>x.Nombre).IsUnique();
+        }
+    }
+}
--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,11 +1,16 @@
+using MagicVilla_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer("name=SqlConnection"));
+
 
 var app = builder.Build();
 
